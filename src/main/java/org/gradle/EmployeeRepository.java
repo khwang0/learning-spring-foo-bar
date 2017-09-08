e255c961c61ac3d6a7f4fd1d83f07a69f2871896
khwang0 <khwang0@gmail.com>
2017-09-08 10:43:42 +0800

    Add comment for EmployeeRepository

--- src/main/java/org/gradle/EmployeeRepository.java
+++ src/main/java/org/gradle/EmployeeRepository.java
@@ -3,6 +3,11 @@ package org.gradle;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 
+//Example modified from spring.io
+
+//This will be AUTO IMPLEMENTED by Spring into a Bean called EmployeeRepository
+//CRUD refers Create, Read, Update, Delete
+
 @Component
 public interface EmployeeRepository extends CrudRepository<Employee, Long>  {
 
