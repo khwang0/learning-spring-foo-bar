215c5a5e66ee0ece482db5c23f35f96e5c5e750a
khwang0 <khwang0@gmail.com>
2017-11-16 13:08:29 +0800

    add travis.yml

--- src/main/java/org/gradle/Employee.java
+++ src/main/java/org/gradle/Employee.java
@@ -8,33 +8,66 @@ import javax.persistence.Id;
 @Entity // This tells Java to make a table out of this class
 public class Employee {
 
+	/**
+	 * @uml.property  name="id"
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+	/**
+	 * @uml.property  name="name"
+	 */
 	private String name;
+	/**
+	 * @uml.property  name="salary"
+	 */
 	private float salary;
 
 	public Employee() {}
 
 	
+	/**
+	 * @param id
+	 * @uml.property  name="id"
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+	/**
+	 * @param name
+	 * @uml.property  name="name"
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	/**
+	 * @param salary
+	 * @uml.property  name="salary"
+	 */
 	public void setSalary(float salary) {
 		this.salary = salary;
 	}
 	// getters and setters
+	/**
+	 * @return
+	 * @uml.property  name="id"
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * @return
+	 * @uml.property  name="name"
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @return
+	 * @uml.property  name="salary"
+	 */
 	public float getSalary() {
 		return salary;
 	}
